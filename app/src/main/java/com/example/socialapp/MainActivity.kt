2ad8f76b6a5c05a916a4509737b40e2241a3d856
Alok Singh <54284060+alokkpsingh123@users.noreply.github.com>
2022-12-06 21:48:23 +0530

    final commit

---
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.socialapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.socialapp.daos.PostDao
+import com.example.socialapp.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.annotation.NonNull
+
+import com.google.android.gms.tasks.OnCompleteListener
+
+
+
+
+class MainActivity : AppCompatActivity(), IPostAdapter {
+
+    private lateinit var postDao: PostDao
+    private lateinit var adapter: PostAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setUpRecyclerView()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        Firebase.auth.signOut()
+
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpRecyclerView() {
+        postDao = PostDao()
+        val postsCollections = postDao.postCollections
+        val query = postsCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLikes(postId)
+    }
+}
